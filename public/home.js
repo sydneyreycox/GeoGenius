2e76cb9c710610eedcfe41c453b632ec3eddc98c
cwalter1 <78448120+cwalter1@users.noreply.github.com>
2026-04-20 11:43:08 -0500

    Add city markers to map from API data
    Fetch city data from API and add markers to the map.

--- public/home.js
+++ public/home.js
@@ -5,7 +5,18 @@ var map = L.map("map", {
   worldCopyJump: true
 })
 
+fetch('/api/cities')
+  .then(response => response.json())
+  .then(data => {
+    data.cities.forEach(city => {
+      L.marker([city.lat, city.lng]).addTo(map)
+        .bindPopup(city.name);
+    });
+  })
+
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+	noWrap: true,
+  maxBoundsViscosity: 1.0,
+  attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
 	maxZoom: 16
 }).addTo(map);
